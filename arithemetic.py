4c8cb209ee771211513dba20c45695889ca4df24
musaib4808 <syedmusaib759@gmail.com>
2026-05-07 16:27:55 +0530

    Add files via upload

---
+++ arithemetic.py
@@ -0,0 +1,6 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b if b != 0 else "Cannot divide by zero")
